0fca550b94bf978be36f6daeab7f083c7bfb7099
yshwang <hysgold@etevers.com>
2023-09-08 09:43:12 +0900

    시험의뢰관리 페이지에서 h1 태그 제거

--- src/app/testRequestManage/page.tsx
+++ src/app/testRequestManage/page.tsx
@@ -1,7 +1,5 @@
 export default function mya () {
     return(
-      <div style={{backgroundColor : 'red', height: '1500px'}}>
-        <h1>Hello</h1>
-      </div>
+      <div style={{backgroundColor : 'red', height: '1500px'}}></div>
     );
   }
